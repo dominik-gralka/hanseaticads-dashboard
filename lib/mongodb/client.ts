aea8c52a850cf2d5f6c2e7c58bf54799d447709c
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2023-09-09 01:41:52 +0200

    Update client.ts

--- lib/mongodb/client.ts
+++ lib/mongodb/client.ts
@@ -1,17 +1,22 @@
-import { MongoClient, Db } from "mongodb"
+import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
 
-let client: MongoClient
-let clientPromise: Promise<MongoClient>  // Explicitly define the type
-
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local")
+if (!uri) {
+  throw new Error("Please add your Mongo URI to .env.local");
 }
 
-client = new MongoClient(uri)
-clientPromise = client.connect()
+const client = new MongoClient(uri);
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
-export default clientPromise
+// Initialize the client and export a module-scoped promise
+const clientPromise = client.connect()
+  .then(() => {
+    console.log("MongoDB connected");
+    return client;
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    throw err; // Rethrow the error to signal a failed connection
+  });
+
+export default clientPromise;
