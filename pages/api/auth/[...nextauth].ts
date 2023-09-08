e626d879e754b8c4347fdb235afdc219a4deb7e7
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2023-09-09 01:50:56 +0200

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,14 +8,7 @@ export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
-        }
-      },
+      server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM
     }),
   ],
